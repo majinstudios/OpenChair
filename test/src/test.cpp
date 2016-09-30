52cf72d32ceb0e18bd04282083ebcc23d90682d8
Irene Sanz Nieto <irenesanznieto@gmail.com>
2016-09-30 11:10:56 +0200

    Maker faire small changes in code, design & logos

--- test/src/test.cpp
+++ test/src/test.cpp
@@ -6,7 +6,7 @@ AnalogIn potX(PA_0);
 AnalogIn potY(PA_1);
 Serial pc(SERIAL_TX, SERIAL_RX);
 
-OpenChair chair(PC_12,PD_2,PC_10,PC_11,50,150);
+OpenChair chair(PC_12,PD_2,PC_10,PC_11,60,150); //50,150
 
 void stopChair( int on_off=1, int ms = 50 ){
   int count = 0;
